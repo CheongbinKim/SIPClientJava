247157267c5f12b3a8f9fbb15cb06860ca7aa40d
cbkim <cheongbin.kim@quantum-ai.ai>
2024-01-23 17:16:38 +0900

    udp sender 추가

--- src/main/java/ai/quantum/qphone/sipclient/UdpMessageHandler.java
+++ src/main/java/ai/quantum/qphone/sipclient/UdpMessageHandler.java
@@ -4,22 +4,27 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.MessagingException;
-import org.springframework.messaging.converter.MessageConverter;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 public class UdpMessageHandler implements MessageHandler {
-    private final MessageConverter messageConverter;
-
-    public UdpMessageHandler(MessageConverter messageConverter) {
-        this.messageConverter = messageConverter;
-    }
-
     @Override
     public void handleMessage(Message<?> message) throws MessagingException {
-        //Object payload = message.getPayload();
-        String payloadAsString = (String) messageConverter.fromMessage(message, String.class);
+        Object payload = message.getPayload();
 
-        log.info(payloadAsString);
+        if (payload instanceof byte[]) {
+            byte[] payloadBytes = (byte[]) payload;
+
+            // Convert byte array to String using a specific character encoding
+            String payloadAsString = new String(payloadBytes, StandardCharsets.UTF_8);
+
+            // Now you can use payloadAsString as needed
+            System.out.println("Received message as String: " + payloadAsString);
+        } else {
+            System.out.println("Received message with non-byte payload");
+        }
+
+        //log.info(message.getPayload().toString());
         //System.out.println("Received UDP message: " + message.getPayload().toString());
     }
 }
