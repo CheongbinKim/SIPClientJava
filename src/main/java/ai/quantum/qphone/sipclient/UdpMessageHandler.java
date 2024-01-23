61408683f8753a11f835ed89c3bc0f04feef1a35
cbkim <cheongbin.kim@quantum-ai.ai>
2024-01-23 17:11:48 +0900

    udp sender 추가

--- src/main/java/ai/quantum/qphone/sipclient/UdpMessageHandler.java
+++ src/main/java/ai/quantum/qphone/sipclient/UdpMessageHandler.java
@@ -4,13 +4,22 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.MessagingException;
+import org.springframework.messaging.converter.MessageConverter;
 
 @Slf4j
 public class UdpMessageHandler implements MessageHandler {
+    private final MessageConverter messageConverter;
+
+    public UdpMessageHandler(MessageConverter messageConverter) {
+        this.messageConverter = messageConverter;
+    }
 
     @Override
     public void handleMessage(Message<?> message) throws MessagingException {
-        log.info(message.getPayload().toString());
+        //Object payload = message.getPayload();
+        String payloadAsString = (String) messageConverter.fromMessage(message, String.class);
+
+        log.info(payloadAsString);
         //System.out.println("Received UDP message: " + message.getPayload().toString());
     }
 }
